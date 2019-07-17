ff634254194be3dd0d81a5d02cc4e2eced751e5e
Nicholas Fonseca <nicholasfonseca@Nicholass-MacBook-Pro.local>
2019-07-16 21:00:47 -0700

    Lesson1

---
+++ SimpleServlet/src/main/java/com/example/demo/MyServlet.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 6632886175268784375L;
+	
+	public void init() throws ServletException {
+		super.init();
+	}
+	
+	public void destroy() {
+		super.destroy();
+	}
+	
+	// Create
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPost was called.");
+    }
+
+    // Read
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet was called.");
+    }
+
+    // Update
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPut was called.");
+    }
+
+    // Delete
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doDelete was called.");
+    }
+}
---
+++ SimpleServlet/src/main/java/com/example/demo/SimpleServletApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan; // is this needed? 
+
+@ServletComponentScan
+@SpringBootApplication
+public class SimpleServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleServletApplication.class, args);
+	}
+
+}
