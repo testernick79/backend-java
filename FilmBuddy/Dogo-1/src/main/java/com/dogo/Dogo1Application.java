1fce19e5e463d3dd41ff64406f755b2bbf063421
Nicholas Fonseca <nicholasfonseca@Nicholass-MacBook-Pro.local>
2019-07-25 10:42:56 -0700

    Lesson5Practice

---
+++ FilmBuddy/Dogo-1/src/main/java/com/dogo/Dogo1Application.java
@@ -0,0 +1,13 @@
+package com.dogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dogo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dogo1Application.class, args);
+	}
+
+}
